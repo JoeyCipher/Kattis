381605e759180d57ec3ffd35a7c3728078005600
Joey Drake <54871710+JoeyCipher@users.noreply.github.com>
2020-01-12 15:12:44 -0600

    Add files via upload

---
+++ quadrant.py
@@ -0,0 +1,16 @@
+x = int(input())
+
+if x > 0:
+   y1 = int(input())
+
+   if y1 > 0:
+      print(1)
+   else:
+      print(4)
+else:
+   y2 = int(input())
+
+   if y2 > 0:
+      print(2)
+   else:
+      print(3)
